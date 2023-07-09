acc36cc651bb67e81e2850b7cf71f42e73581f47
aminul-islam-niloy <niloyftaminul@gmail.com>
2023-07-09 13:15:12 +0600

    memory allocation methods for fixed partition using first fit,best,worst

---
+++ memory allocation methods/09_first_fit.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int hole;
+    cout << "Enter the number of free memory holes: ";
+    cin >> hole;
+    
+    vector<int> holes(hole);
+    cout << "Enter the size of each memory hole:\n";
+    for (int i = 0; i < hole; i++)
+    {
+        cout << "Hole " << i+1 << ": ";
+        cin >> holes[i];
+    }
+
+    int size;
+    cout << "Enter the process size: ";
+    cin >> size;
+
+    for (int x : holes)
+    {
+        if (x >= size)
+        {
+            cout << "The process is allocated into " << x << " MB memory hole.";
+            return 0;
+        }
+    }
+    cout << "No suitable hole available!";
+    return 0;
+}
---
+++ memory allocation methods/11_worst_fit.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// bool compareDescending(int a, int b) {
+//     return a > b; 
+// }
+
+int main()
+{
+    int hole;
+    cout << "Enter the number of free memory holes: ";
+    cin >> hole;
+    
+    vector<int> holes(hole);
+    cout << "Enter the size of each memory hole:\n";
+    for (int i = 0; i < hole; i++)
+    {
+        cout << "Hole " << i+1 << ": ";
+        cin >> holes[i];
+    }
+
+    int size;
+    cout << "Enter the process size: ";
+    cin >> size;
+
+   // sort(holes.begin(), holes.end(), compareDescending);
+    sort(holes.begin(), holes.end(), greater<int>());
+    for (int x : holes)
+    {
+    //    if (holes.back() >= size)
+        if (x >= size)
+        {
+            cout << "The process is allocated into " << x << " MB memory hole.";
+            return 0;
+        }
+    }
+    cout << "No suitable hole available!";
+    return 0;
+}
